28903829b7feaa3ec042ee5b93e0234da56d8159
DhyanBsh <dyanbashitha5@gmail.com>
2025-02-06 20:05:53 +0530

    Updated About

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./About.css";
 import Technologies from "../Technologies/Technologies";
-import DesigningTools from "../DesigningTools/DesigningTools";
+//import DesigningTools from "../DesigningTools/DesigningTools";
 
 
 const About = () => {
@@ -57,82 +57,7 @@ const About = () => {
         </div>
       </div>
 
-      <div className="about-section">
-        <div className="about-para">
-          <h1>Graphic Designing</h1>
-          <p>
-            I design attractive, simple, and clean UI interfaces, along with creative and detailed graphics such as flyer designs, banner designs, event screens, logo designs, and card designs. In addition to graphic work, I also produce engaging and creative videos that bring ideas to life through visual storytelling.
-          </p>
-        </div>
-
-        <div>
-          <DesigningTools />
-        </div>
-
-        <div className="about-skills">
-          <div className="about-skill">
-            <p>Adobe Photoshop</p>
-            <hr style={{ width: "90%" }} />
-            <span className="percentage">90%</span>
-          </div>
-          <div className="about-skill">
-            <p>Adobe Illustrator</p>
-            <hr style={{ width: "70%" }} />
-            <span className="percentage">70%</span>
-          </div>
-          <div className="about-skill">
-            <p>Adobe Lightroom</p>
-            <hr style={{ width: "80%" }} />
-            <span className="percentage">80%</span>
-          </div>
-          <div className="about-skill">
-            <p>Canva</p>
-            <hr style={{ width: "100%" }} />
-            <span className="percentage">100%</span>
-          </div>
-          <div className="about-skill">
-            <p>Adobe Premiere Pro</p>
-            <hr style={{ width: "90%" }} />
-            <span className="percentage">90%</span>
-          </div>
-          <div className="about-skill">
-            <p>Adobe After Effects</p>
-            <hr style={{ width: "80%" }} />
-            <span className="percentage">80%</span>
-          </div>
-          <div className="about-skill">
-            <p>DaVinci Resolve</p>
-            <hr style={{ width: "60%" }} />
-            <span className="percentage">60%</span>
-          </div>
-          <div className="about-skill">
-            <p>Blender</p>
-            <hr style={{ width: "50%" }} />
-            <span className="percentage">50%</span>
-          </div>
-        </div>
-      </div>
-
-      <div className="about-achievements">
-        <div className="about-achievement">
-          <h1>03+</h1>
-          <p>Years of Experience</p>
-        </div>
-        <hr />
-        <div className="about-achievement">
-          <h1>15+</h1>
-          <p>Visual Creation Projects</p>
-        </div>
-        <hr />
-        <div className="about-achievement">
-          <h1>25+</h1>
-          <p>Designing Projects</p>
-        </div>
-      </div>
-
-      <div>
-          
-        </div>
+    
     </div>
   );
 };
